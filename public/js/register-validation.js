c4d7a61ea29587beb1b31c537fef1be82a4a7ebf
amutuheiremellon3@gmail.com <amutuheiremellon3@gmail.com>
2023-07-12 08:27:57 +0300

    Addition of my booking page

--- public/js/register-validation.js
+++ public/js/register-validation.js
@@ -1,86 +1,89 @@
-const Validate=()=>{
-// picking input fields with their names
-let firstName = document.register.firstname
-let lastName = document.register.lastname
-let sex = document.register.Sex;
-let carNumberPlate = document.register.CarNumberPlate;
-let phoneNumber = document.register.phonenumber 
+const Validate = () => {
+  // picking input fields with their names
+  let firstName = document.register.firstname;
+  let lastName = document.register.lastname;
+  let email = document.register.email;
+  let carNumberPlate = document.register.carnumberplate;
+  let phoneNumber = document.register.phonenumber;
+  let sex = document.register.sex;
 
+  // picking error fields
+  let firstNameError = document.getElementById("firstnameError");
+  let lastNameError = document.getElementById("lastnameError");
+  let emailError = document.getElementById("pnameError");
+  let errorCarNumberPlate = document.getElementById("gnameError");
+  let phoneNumberError = document.getElementById("phonenumberError");
+  let sexError = document.getElementById("qnameError");
 
+  // validating  firstname input
+  // validating for emptiness
+  if (firstName.value == "") {
+    firstName.style.border = "2px solid red";
+    firstNameError.textContent = "please provide your firstname";
+    firstNameError.style =
+      "color: red; fontsize:11px; font-size:Helvetical, Arial,sans-serif";
+    firstName.focus();
+    return false;
+  } else if (firstName.value.length < 2) {
+    firstName.style.border = "2px solid red";
+    firstNameError.textContent = "first name must be at least 2 characters";
+    firstNameError.style =
+      "color: red; fontsize:11px; font-size:Helvetical, Arial,sans-serif";
+    firstName.focus();
+    return false;
+  } else if (firstName.value.length > 15) {
+    firstName.style.border = "2px solid red";
+    firstNameError.textContent =
+      "first name must not be greate than 15 characters";
+    firstNameError.style =
+      "color: red; fontsize:11px; font-size:Helvetical, Arial,sans-serif";
+    firstName.focus();
+    return false;
+  } else {
+    firstName.style.border = "2px solid green";
+    firstNameError.textContent = "";
+    lastName.focus();
+  }
 
+  // validating phone number
 
+  if (phoneNumber.value == "") {
+    phoneNumber.style.border = "2px solid red";
+    phoneNumberError.textContent = "please provide your phone number";
+    phoneNumberError.style =
+      "color: red; fontsize:11px; font-size:Helvetical, Arial,sans-serif";
+    phoneNumber.focus();
+    return false;
+  }
 
+  let phoneRegex = /^[+][2][5][6][0-9]{9}$/;
 
-// picking error fields
-let firstNameError = document.getElementById("firstnameError");
-let lastNameError = document.getElementById("lastnameError");
-let erroSex = document.getElementById("qnameError");
-let errorCarNumberPlate = document.getElementById("gnameError");
-let PhoneNumberError = document.getElementById("phonenumberError");
+  if (!phoneRegex.test(phoneNumber.value)) {
+    phoneNumber.style.border = "1px solid red";
+    phoneNumberError.textContent = "Phone number should start with +256";
+    phoneNumberError.style =
+      "color: red; font-size:11px; font-family:helvetica,Arial,sans-serif;";
+    phoneNumber.focus();
+    return false;
+  }
+  // validation for Email
+  if (email.value == "") {
+    email.style.border = "1px solid red";
+    emailError.textContent = "Email `is required";
+    emailError.style =
+      "color: red; font-size:11px; font-family:helvetica,Arial,sans-serif;";
+    email.focus();
+    return false;
+  }
+  let EmailRegex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  
-// validating  firstname input
-// validating for emptiness
-if(firstName.value ==""){
-  firstName.style.border = "2px solid red";
-  firstNameError.textContent = "please provide your firstname";
-  firstNameError.style = "color: red; fontsize:11px; font-size:Helvetical, Arial,sans-serif";
-  firstName.focus();
-  return false;
-}
-else if (firstName.value.length <2){
-  firstName.style.border = "2px solid red";
-  firstNameError.textContent = "first name must be at least 2 characters";
-  firstNameError.style = "color: red; fontsize:11px; font-size:Helvetical, Arial,sans-serif";
-  firstName.focus();
-  return false;
-}
-else if (firstName.value.length > 15) {
-  firstName.style.border = "2px solid red";
-  firstNameError.textContent = "first name must not be greate than 15 characters";
-  firstNameError.style = "color: red; fontsize:11px; font-size:Helvetical, Arial,sans-serif";
-  firstName.focus();
-  return false;
-}
-else {
-  firstName.style.border = "2px solid green";
-  firstNameError.textContent = "";
-  lastName.focus();
-  
-}
-
-
-
-// vaidating phone number
-// if(phone.value == ";"){
-//   phone.style.border = "2px solid red";
-//   PhoneError.textContent = "please provide your phone number";
-//   phoneError.style = "color: red; fontsize:11px; font-size:Helvetical, Arial,sans-serif";
-//   phone.focus();
-//   return false;
-// }
-
-
-
-
-// let phoneRegex = /^+256\d{9}$/;
-
-// if(!phoneRegex.test(phone.value)){
-//   phone.style.border = "2px solid red";
-//   PhoneError.textContent = "please provide a valid phone number";
-//   phoneError.style = "color: red; fontsize:11px; font-size:Helvetical, Arial,sans-serif";
-//   phone.focus();
-//   return false;
-// }
-
-// // email address validation
-// let emailRegex = 
-// if (email.value){
-
-// }
-
-
-
-
-
-}
+  if (!email.value.match(EmailRegex)) {
+    email.style.border = "1px solid red";
+    emailError.textContent = "The email address should be valid";
+    emailError.style =
+      "color: red; font-size:11px; font-family:helvetica,Arial,sans-serif;";
+    email.focus();
+    return false;
+  }
+};
